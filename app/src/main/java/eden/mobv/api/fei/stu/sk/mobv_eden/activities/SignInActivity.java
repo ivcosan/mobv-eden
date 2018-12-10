f7b7f44ad08017378f16970b73fbef0c6d539bc6
Emilia Kytkova <milka.brisova@gmail.com>
2018-12-10 21:48:35 +0100

    pridany listener na dotiahnutie usera z db

--- app/src/main/java/eden/mobv/api/fei/stu/sk/mobv_eden/activities/SignInActivity.java
+++ app/src/main/java/eden/mobv/api/fei/stu/sk/mobv_eden/activities/SignInActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -61,7 +62,9 @@ public class SignInActivity extends AppCompatActivity {
                 if (response.isNewUser()) {
                     addNewUserWithPosts();
                 } else {
-                    Toast.makeText(getBaseContext(), "prihlaseny", Toast.LENGTH_LONG).show();
+                    FirestoreDatabase fd = new FirestoreDatabase();
+                    fd.getDataFromUserDocument();
+//                    Toast.makeText(getBaseContext(), "prihlaseny", Toast.LENGTH_LONG).show();
                 }
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(intent);
--- app/src/main/java/eden/mobv/api/fei/stu/sk/mobv_eden/resources/FirestoreDatabase.java
+++ app/src/main/java/eden/mobv/api/fei/stu/sk/mobv_eden/resources/FirestoreDatabase.java
@@ -1,26 +1,41 @@
 package eden.mobv.api.fei.stu.sk.mobv_eden.resources;
 
 import android.support.annotation.NonNull;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.crashlytics.android.Crashlytics;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+import eden.mobv.api.fei.stu.sk.mobv_eden.activities.MainActivity;
+
 public class FirestoreDatabase {
+    public interface FirestoreDatabaseListener {
+        public void onDataLoaded();
+    }
 
     private FirebaseFirestore database;
+    private FirestoreDatabaseListener listener;
 
     public FirestoreDatabase() {
+        this.listener = null;
         database = FirebaseFirestore.getInstance();
+    }
+
+    public void setFirestoreDatabaseListener(FirestoreDatabaseListener listener) {
+        this.listener = listener;
     }
 
     public void addPost(String mediaType, String mediaUrl, String userId, String username) {
@@ -83,17 +98,43 @@ public class FirestoreDatabase {
 
 
     // TODO: treba overit funkcnost
-    private void updateUser() {
+    public void updateUser() {
         FirebaseAuth auth = FirebaseAuth.getInstance();
         FirebaseUser firebaseUser = auth.getCurrentUser();
 
         User user = User.getInstance();
         int newNumberOfPosts = user.getNumberOfPosts() + 1;
+        Log.i("macko", User.getInstance().getUsername());
 
         Map<String, Object> updatePosts = new HashMap<>();
         updatePosts.put("numberOfPosts", newNumberOfPosts);
         this.database.collection("users")
                 .document(firebaseUser.getUid())
                 .update(updatePosts);
+    }
+
+    public void getDataFromUserDocument() {
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        final DocumentReference docRef = this.database.collection("users").document(auth.getCurrentUser().getUid());
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Map<String, Object> data = document.getData();
+                        User.getInstance().setUsername(data.get("username").toString());
+                        User.getInstance().setNumberOfPosts(((Long)data.get("numberOfPosts")).intValue());
+                        if (listener != null) {
+                            listener.onDataLoaded();
+                        }
+                    } else {
+                        Crashlytics.log("User document -> no such document");
+                    }
+                } else {
+                    Crashlytics.logException(task.getException());
+                }
+            }
+        });
     }
 }
