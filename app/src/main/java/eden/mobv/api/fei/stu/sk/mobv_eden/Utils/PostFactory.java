a3e5c6eb5bb1563f5ba2ac6f1b14bd831202aa97
Emilia Kytkova <milka.brisova@gmail.com>
2018-12-13 00:24:30 +0100

    zmenena icona appky

--- app/src/main/java/eden/mobv/api/fei/stu/sk/mobv_eden/Utils/PostFactory.java
+++ app/src/main/java/eden/mobv/api/fei/stu/sk/mobv_eden/Utils/PostFactory.java
@@ -9,20 +9,20 @@ import java.util.List;
 
 public class PostFactory {
 
-    public static List<ParentPost> getRandomParents() {
-        List<ParentPost> result = new ArrayList<>();
-        for(int i = 0; i < 10; i++){
-            result.add(new ParentPost("Parent title: "+i, getRandomChildren()));
-        }
-        return result;
-    }
+//    public static List<ParentPost> getRandomParents() {
+//        List<ParentPost> result = new ArrayList<>();
+//        for(int i = 0; i < 10; i++){
+//            result.add(new ParentPost("Parent title: "+i, getRandomChildren()));
+//        }
+//        return result;
+//    }
 
-    public static List<ChildPost> getRandomChildren() {
-        List<ChildPost> result = new ArrayList<>();
-        for(int i = 0; i < 10; i++){
-            result.add(new ChildPost("Child title: "+i, R.mipmap.letter_i));
-        }
-        return result;
-    }
+//    public static List<ChildPost> getRandomChildren() {
+//        List<ChildPost> result = new ArrayList<>();
+//        for(int i = 0; i < 10; i++){
+//            result.add(new ChildPost("Child title: "+i, R.mipmap.letter_i));
+//        }
+//        return result;
+//    }
 
 }
